72f360b456abcc231780605c7eaba1445211fdb2
Patric Angly <padde95@hotmail.se>
2023-01-19 07:33:14 +0100

    Add service and game-component

---
+++ src/app/gameboard/gameboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gameboard',
+  template: `
+    <p>
+      gameboard works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GameboardComponent {
+
+}

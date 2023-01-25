6609efc05b26acef701b332c1a9f8ec2b9117cf6
Patric Angly <padde95@hotmail.se>
2023-01-25 21:51:21 +0100

    add name validation

--- src/app/user-modal/user-modal.component.ts
+++ src/app/user-modal/user-modal.component.ts
@@ -6,13 +6,16 @@ import { GameService } from '../game.service';
   selector: 'app-user-modal',
   template: `
   <div class="modal-body">
-    <form (ngSubmit)="saveName()">
+    <form #myForm="ngForm" (ngSubmit)="saveName()">
       <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" class="form-control" [(ngModel)]="this.gameService.newPlayer.name" name="name" required>
+        <input type="text" class="form-control" [(ngModel)]="this.gameService.newPlayer.name" name="name" required required minlength="3" #name="ngModel">
+        <div *ngIf="name.invalid && name.touched" class="alert alert-danger">
+      Name must be at least 3 characters long.
+    </div>
   </div>
   <div class="modal-footer">
-    <button type="submit" class="btn btn-primary">Start game</button>
+    <button type="submit" class="btn btn-primary" [disabled]="!myForm.valid">Start game</button>
   </div>
   </form>
   `,
